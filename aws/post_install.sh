90670ac31a425ce1c526b166398eeeca7e1ceb77
Stefan <hoitaus@gmail.com>
2016-10-17 14:02:13 +0800

    todo install netcdf on nodes

--- aws/post_install.sh
+++ aws/post_install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# On centos ami-d5f52eb6, you can't' serve directories with sshfs. Fix by softlinking
-sudo ln -s /usr/libexec/openssh/sftp-server /usr/libexec/sftp-server
-
 sudo yum -y install netcdf-fortran-devel
 wget https://s3-ap-southeast-1.amazonaws.com/romsaws/roms.tar.gz
 tar -xf roms.tar.gz
 wget https://github.com/poidl/awsroms/archive/master.tar.gz
 tar -xf master.tar.gz
+
+#TODO install netcdf on nodes
+
+# On centos ami-d5f52eb6, you can't' serve directories with sshfs. Fix by softlinking
+sudo ln -s /usr/libexec/openssh/sftp-server /usr/libexec/sftp-server
