7a0563b5bd1e37a5e7ce7660a676b9e86517e973
Stefan <hoitaus@gmail.com>
2016-10-31 15:34:24 +0800

    script to move results to s3

--- aws/make_config.py
+++ aws/make_config.py
@@ -14,8 +14,14 @@ TEMPLATE = './templates/config_template'
 OUTPUT_FILE = PRIVATE_DIR+'/config'
 ft.fill(VARS, TEMPLATE, OUTPUT_FILE)
 
-# make the post_install file
+# post_install file
 
 TEMPLATE = './templates/post_install_template'
 OUTPUT_FILE = PRIVATE_DIR+'/post_install.sh'
+ft.fill(VARS, TEMPLATE, OUTPUT_FILE)
+
+# utilities
+
+TEMPLATE = './templates/move_s3_template'
+OUTPUT_FILE = PRIVATE_DIR+'/move_s3.sh'
 ft.fill(VARS, TEMPLATE, OUTPUT_FILE)
