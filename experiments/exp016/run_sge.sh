fdc0f3f43f49b219f900a63b50995ff2d8bb8a36
Stefan <hoitaus@gmail.com>
2017-01-04 10:04:11 +1000

    benchmark2

---
+++ experiments/exp016/run_sge.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#
+
+mkdir -p nc/out
+
+MPI_DIR=/usr/lib64/openmpi
+NSLOTS=4
+MACHINEFILE=machinefile
+
+$MPI_DIR/bin/mpirun -np $NSLOTS oceanM ocean_benchmark2.in
+        
---
+++ experiments/exp021/run.sh
@@ -0,0 +1,1 @@
+/usr/lib64/openmpi/bin/mpirun -n 2 -machinefile ./machinefile oceanM ocean_upwelling.in
